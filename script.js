4a30db8912888fbeba8818b9212dc2cc1626f567
Nweke Blessing Chioma <nwekeblessing59@gmail.com>
2024-01-28 16:51:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+//number 1
+function findSquare(square) {
+  return square * 2;
+}
+console.log(findSquare(16));
+
+//number 2
+const calFahrenheit = function (celsius) {
+  return (celsius * 9) / 5 + 32;
+};
+console.log(calFahrenheit(50));
+
+//number 3
+const circleValues = (radius) => 2 * 3.14 * radius; //perimeter
+console.log(circleValues(12));
+
+const circleValue = (radius) => 3.14 * radius * radius; //area
+console.log(circleValue(12));
+
+//number 4
+function simpleInt(interest, principalAmount, time) {
+  return (interest * principalAmount * time) / 100;
+}
+console.log(simpleInt(50, 25, 10));
+
+//number 5
+const rectangleArea = function (width, height) {
+  return width * height;
+};
+console.log(rectangleArea(15, 10));
+
+//number 6
+console.log("true and false values");
